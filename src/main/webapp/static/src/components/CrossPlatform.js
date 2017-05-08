57379f2c6a088aa21f7c76b44e3d458cab26b2de
m2mbob <2844870322@qq.com>
2017-05-08 12:58:33 +0800

    crossplatform

--- src/main/webapp/static/src/components/CrossPlatform.js
+++ src/main/webapp/static/src/components/CrossPlatform.js
@@ -9,13 +9,17 @@ export default class CrossPlatform extends PureComponent {
         return (
             <div>
                 <Row>
-                    <Col span={12}>
+                    <Col span={8}>
                         <h1 style={{color: '#f04134'}}><Icon type="android" />Android</h1>
-                        <iframe src="https://appetize.io/embed/h3x3cuxmwmcv8mqpnhgj15cu30?device=nexus5&scale=100&orientation=portrait&osVersion=7.0" width="407px" height="800px" style={{border: 'none'}} scrolling="no"/>
+                        <iframe src="https://appetize.io/embed/h3x3cuxmwmcv8mqpnhgj15cu30?device=nexus5&scale=75&orientation=portrait&osVersion=7.0" width="306px" height="600px" style={{border: 'none'}} scrolling="no"/>
                     </Col>
-                    <Col span={12}>
+                    <Col span={8}>
                         <h1 style={{color: '#108ee9'}}><Icon type="apple" />iOS</h1>
-                        <iframe src="https://appetize.io/embed/wfjuz7nfk98nk3pz04vk8ugqxr?device=iphone6&scale=100&orientation=portrait&osVersion=10.3" width="422px" height="873px" style={{border: 'none'}} scrolling="no"/>
+                        <iframe src="https://appetize.io/embed/wfjuz7nfk98nk3pz04vk8ugqxr?device=iphone6&scale=75&orientation=portrait&osVersion=10.3" width="317px" height="655px" style={{border: 'none'}} scrolling="no"/>
+                    </Col>
+                    <Col span={8}>
+                        <h1 style={{color: '#00a854'}}><Icon type="chrome" />Web</h1>
+                        <iframe src="https://appetize.io/embed/h3x3cuxmwmcv8mqpnhgj15cu30?device=nexus5&scale=75&orientation=portrait&osVersion=7.0" width="317px" height="655px" style={{border: 'none'}} scrolling="no"/>
                     </Col>
                 </Row>
             </div>
